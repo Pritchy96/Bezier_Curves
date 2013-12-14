9c4153e59fd0e8551dba3418e39b6cfd44f02e55
Tom Pritchard <tompritchard1996@gmail.com>
2013-12-14 02:43:48 +0000

    Getting... somewhere..

--- Bezier/States/MainState.cs
+++ Bezier/States/MainState.cs
@@ -10,15 +10,18 @@ using System.Windows.Forms;
 class MainState : BasicState
 {
     //The three control points from which to draw a Bezier curve.
-    Point p1 = new Point(10, 10);
-    Point p2 = new Point(10, 300);
-    Point p3 = new Point(300, 300);
+    Point p1 = new Point(300, 10);
+    Point p2 = new Point(10, 10);
+    Point p3 = new Point(10, 300);
 
     //The current point being drawn.
     Point currentPoint = new Point();
 
     //Array of calculated points.
     ArrayList points = new ArrayList();
+    ArrayList controlPoints = new ArrayList();
+
+    Rectangle screenSize = new Rectangle(0, 0, 1200, 900);
 
     //Incrementer.
     float i = 0;
@@ -27,26 +30,18 @@ class MainState : BasicState
     Point line1;
     Point line2;
 
-    //The rectangles of the control points.
-    Rectangle p1Rect;
-    Rectangle p2Rect;
-    Rectangle p3Rect;
-
     bool mouseDown = false;
 
     public MainState()
     {
-        //Setting up rectangles initially.
-        p1Rect = new Rectangle((int)p1.X - 4, (int)p1.Y - 4, 8, 8);
-        p2Rect = new Rectangle((int)p2.X - 4, (int)p2.Y - 4, 8, 8);
-        p3Rect = new Rectangle((int)p3.X - 4, (int)p3.Y - 4, 8, 8);
+
+        controlPoints.Add(p1);
+        controlPoints.Add(p2);
+        controlPoints.Add(p3);
     }
 
-    public void CalculateBezier()
+    public Point CalculateBezierPoint(Point p1, Point p2, Point p3, float i)
     {
-        //Incrementing i.
-        i += 0.01f;
-
         // Calculating guide line coordinates. It uses i to simply move along each line 
         //connecting two of the control coordinates linearly.
         line1 = getPoint(p1, p2, i);
@@ -54,20 +49,49 @@ class MainState : BasicState
 
         // Moves along the guide line by multiplying by i.
         currentPoint = getPoint(line1, line2, i);
-        points.Add(currentPoint);
+
+        return currentPoint;
     }
 
     public override void Update()
     {
+       
+    }
+
+
+    public void NewCurve(Point p1, Point p2, Point p3)
+    {
+        controlPoints.Add(p1);
+        controlPoints.Add(p2);
+        controlPoints.Add(p3);
+
         //IF we are not at the end, calculate another few points.
         //i is the end because it is when the second coordinate for the
         //guide line is at the last control point, and the current point
         // rectangle is at the end of that line.
-        if (i < 3)
+        while (i < 2 && screenSize.Contains(currentPoint))
         {
-                CalculateBezier();
-                CalculateBezier();
-                CalculateBezier();
+            i += 0.1f;
+            CalculateBezierPoint(p1, p2, p3, i);
+
+            if (currentPoint.Y >= 700)
+            {
+                int x = (currentPoint.X / currentPoint.Y) * 10;
+                double angle1 = Math.Tanh(currentPoint.Y / currentPoint.X);
+                int y = 100;
+
+                Point b = new Point(currentPoint.X + 100, 500);
+                Point c = new Point(currentPoint.X + 200, 600);
+
+                i = 0;
+
+                NewCurve(currentPoint, b, c);
+            }
+            else
+            {
+
+                points.Add(currentPoint);
+            }
         }
     }
 
@@ -78,13 +102,14 @@ class MainState : BasicState
             p1 = e.Location;
             i = 0;
             points.Clear();
-            p1Rect = new Rectangle((int)p1.X - 4, (int)p1.Y - 4, 8, 8);
-            p2Rect = new Rectangle((int)p2.X - 4, (int)p2.Y - 4, 8, 8);
-            p3Rect = new Rectangle((int)p3.X - 4, (int)p3.Y - 4, 8, 8);
+            controlPoints.Clear();
 
             p2 = getPoint(p1, p3, 0.5f);
             p2.Y -= Math.Abs(p1.X - p3.X)/4;
         }
+
+
+        NewCurve(p1, p2, p3);
     }
 
     public override void MouseUp(MouseEventArgs e)
@@ -94,6 +119,7 @@ class MainState : BasicState
 
     public override void MouseDown(MouseEventArgs e)
     {
+            
             p3 = e.Location;
             mouseDown = true;
     }
@@ -101,9 +127,10 @@ class MainState : BasicState
     public override void Redraw(PaintEventArgs e)
     {
         //Draw the control point rectangles.
-        e.Graphics.DrawRectangle(Pens.Black, p1Rect);
-        e.Graphics.DrawRectangle(Pens.Black, p2Rect);
-        e.Graphics.DrawRectangle(Pens.Black, p3Rect);
+        foreach (Point p in controlPoints)
+        {
+            e.Graphics.DrawRectangle(Pens.Black, new Rectangle((int)p.X - 4, (int)p.Y - 4, 8, 8));
+        }
 
         //Connect the control Points to make it look pretty.
         e.Graphics.DrawLine(Pens.Black, p1, p2);
@@ -118,10 +145,9 @@ class MainState : BasicState
         //Draw every pixel in the array of pixels making up the line.
         foreach (Point p in points.ToArray())
         {
-            e.Graphics.DrawRectangle(Pens.Orange, new Rectangle((int)p.X, (int)p.Y, 1, 1));
+            e.Graphics.DrawRectangle(Pens.Red, new Rectangle((int)p.X, (int)p.Y, 1, 1));
         }
     }
-
 
     public Point getPoint(Point point1, Point point2, float perc)
     {
